1ddc3cedf389232e4018c5113f9f27ec975ec4c3
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-02-28 09:26:53 +0100

    lab02zad02

---
+++ lab02/lab02zad02.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (fibonacci n)
+    (cond 
+        ((= n 0) 0)
+        ((= n 1) 1)
+        (else 
+            (+ 
+                (fibonacci (- n 1)) 
+                (fibonacci (- n 2))))))
+
+(define (helper_fibonacci n i before current)
+    (cond 
+        ((= n 0) 0)
+        ((= n 1) 1)
+        ((= n i) current)
+        (else 
+            (helper_fibonacci n (+ i 1) current (+ current before)))))
+
+(define (better_fibonacci n)
+    (helper_fibonacci n 1 0 1))
