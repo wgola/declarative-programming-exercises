f8e5392cda01dccf26214ff4bff40c94d3ab416a
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-03-01 19:32:47 +0100

    refactored lab02zad02

--- lab02/lab02zad02.rkt
+++ lab02/lab02zad02.rkt
@@ -9,13 +9,12 @@
                 (fibonacci (- n 1)) 
                 (fibonacci (- n 2))))))
 
-(define (helper_fibonacci n i before current)
-    (cond 
-        ((= n 0) 0)
-        ((= n 1) 1)
-        ((= n i) current)
-        (else 
-            (helper_fibonacci n (+ i 1) current (+ current before)))))
-
 (define (better_fibonacci n)
+    (define (helper_fibonacci n i before current)
+        (cond 
+            ((= n 0) 0)
+            ((= n 1) 1)
+            ((= n i) current)
+            (else 
+                (helper_fibonacci n (+ i 1) current (+ current before)))))
     (helper_fibonacci n 1 0 1))
