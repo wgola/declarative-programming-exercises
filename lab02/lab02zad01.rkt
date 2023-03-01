dcad5bd6c18ae711854f23066e8e304fb35ead28
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-03-01 19:32:10 +0100

    refactored lab02zad01

--- lab02/lab02zad01.rkt
+++ lab02/lab02zad01.rkt
@@ -1,14 +1,15 @@
 #lang racket
 
+;zadanie 6
+
 (define (factorial n) 
     (if (> n 1) 
         (* n (factorial (- n 1))) 
             1 ))
 
-(define (factorial_help n i acc)
-    (if (< n i)
-        acc
-        (factorial_help n (+ i 1) (* i acc))))
-
 (define (better_factorial n)
+    (define (factorial_help n i acc)
+        (if (< n i)
+            acc
+            (factorial_help n (+ i 1) (* i acc))))
     (factorial_help n 1 1))
