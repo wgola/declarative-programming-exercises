5928919f6c7722bb98ee1998883f26813c7fd390
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-04-17 13:44:14 +0200

    lab06zad02

---
+++ lab06/lab06zad02.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;zadanie 26
+
+(define (fibonacci n)
+    (define (multiply-matrix m1 m2)
+        (list 
+            (+ (* (list-ref m1 0) (list-ref m2 0)) (* (list-ref m1 1) (list-ref m2 2)))
+            (+ (* (list-ref m1 0) (list-ref m2 1)) (* (list-ref m1 1) (list-ref m2 3)))
+            (+ (* (list-ref m1 2) (list-ref m2 0)) (* (list-ref m1 3) (list-ref m2 2)))
+            (+ (* (list-ref m1 2) (list-ref m2 1)) (* (list-ref m1 3) (list-ref m2 3)))    
+        )
+    )
+    (define (helper x)
+        (if (eq? x 0)
+            `(1 1 1 0)
+            (multiply-matrix `(1 1 1 0) (helper (- x 1)))
+        )
+    )
+    (list-ref (helper n) 3)
+)
