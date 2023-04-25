43c3ce1755199ee51a65d8b3de2b201006948dfb
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-04-25 09:39:30 +0200

    lab08zad02

---
+++ lab08/lab08zad02.pl
@@ -0,0 +1,6 @@
+child(X, Y) :- parent(Y, X).
+mother(X, Y) :- parent(X, Y), female(X).
+sister(X, Y) :- female(X), parent(X, Z), parent(Y, Z).
+has_a_child(X) :- parent(X, _).
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+predecessor(X, Y) :- child(X, Z), child(Z, Y).
